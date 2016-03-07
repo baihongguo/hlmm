2f5efdf96ac6fb8fbb09749dfe147bc7d308bb03
ay <alextisyoung@gmail.com>
2016-03-07 12:59:18 +0000

    interact

---
+++ kill_q_jobs.py
@@ -0,0 +1,19 @@
+import os,argparse
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument('qfile',type=str)
+
+args=parser.parse_args()
+
+qfile=file(args.qfile,'r')
+
+qline=qfile.readline()
+
+while len(qline)>0:
+    qline=qline.split(' ')
+    job_id=qline[0]
+    os.systeme('qdel '+job_id)
+    qline=qfile.readline()
+
+qfile.close()
