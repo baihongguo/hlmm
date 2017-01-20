d8a710734b1c92ec912de5ebf03fce7fb83bdda3
ay <alextisyoung@gmail.com>
2017-01-20 14:10:18 +0000

    interact

--- linear_heteroskedastic_mixed_model_bed.py
+++ linear_heteroskedastic_mixed_model_bed.py
@@ -684,7 +684,7 @@ if __name__ == "__main__":
                         default=0)
     parser.add_argument('--min_maf',type=float,help='Minimum minor allele frequency',default=0.05)
     parser.add_argument('--max_missing',type=float,help='Maximum percent of missing genotype calls',default=5)
-    parser.add_argument('--missing_char',type=str,help='Missing value string in phenotype file',defaul='NA')
+    parser.add_argument('--missing_char',type=str,help='Missing value string in phenotype file',default='NA')
 
     args=parser.parse_args()
 
