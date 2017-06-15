6bc34b50c42f133a2dc02d89450b65cf1c2b9c1f
ay <alextisyoung@gmail.com>
2017-06-15 14:20:48 +0100

    interact

--- pheno_sim_AV.R
+++ pheno_sim_AV.R
@@ -14,7 +14,7 @@ g=g[,1]
 add_effect=sqrt(2*as.numeric(h2))
 
 y=replicate(1000,sapply(g,function(x) rnorm(1,add_effect*x,exp(as.numeric(v)*x))))
-
+print('Simulated phenotypes')
 h5createFile(paste('/well/donnelly/ukbiobank_project_8874/ay/linear_variance/new_simulations/av_vs_a/h2_',h2,'_v_',v,'.hdf5',sep=''))
 h5write(y,paste('/well/donnelly/ukbiobank_project_8874/ay/linear_variance/new_simulations/av_vs_a/h2_',h2,'_v_',v,'.hdf5',sep=''),'phenotypes')
 h5write(as.character(1:10^5),paste('/well/donnelly/ukbiobank_project_8874/ay/linear_variance/new_simulations/av_vs_a/h2_',h2,'_v_',v,'.hdf5',sep=''),'sample_id')
