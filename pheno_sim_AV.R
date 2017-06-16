58b80699dfaff7bf5dcaf0e6152b2671e8026f20
ay <alextisyoung@gmail.com>
2017-06-16 14:03:04 +0100

    interact

--- pheno_sim_AV.R
+++ pheno_sim_AV.R
@@ -5,6 +5,20 @@ v=args[1]
 
 h2=args[2]
 
+h2_d=args[3]
+
+gvar=args[4]
+
+dom_gt_make=function(g,f){
+    if (g==0){
+        return(f^2)
+    } else if (g==1){
+    return(-f*(1-f))
+} else if (g==2){
+        return((1-f)^2)
+    }
+}
+
 library(rhdf5)
 
 g=h5read('/well/donnelly/ukbiobank_project_8874/ay/linear_variance/new_simulations/artificial_gt_0.5.hdf5','genotypes')
@@ -13,7 +27,15 @@ g=g[,1]
 
 add_effect=sqrt(2*as.numeric(h2))
 
-y=replicate(1000,sapply(g,function(x) rnorm(1,add_effect*x,exp(as.numeric(v)*x))))
+f=mean(g)/2
+
+dom_gts=sapply(g,dom_gt_make,f)
+
+dom_effect=sqrt(as.numeric(h2_d))/(f*(1-f))
+
+
+
+y=replicate(1000,sapply(g,function(x) rnorm(1,add_effect*x+dom_effect*dom_gts,exp(as.numeric(v)*x+as.numeric(gvar)*dom_gts))))
 
 h5createFile(paste('/well/donnelly/ukbiobank_project_8874/ay/linear_variance/new_simulations/av_vs_a/h2_',h2,'_v_',v,'.hdf5',sep=''))
 h5write(y,paste('/well/donnelly/ukbiobank_project_8874/ay/linear_variance/new_simulations/av_vs_a/h2_',h2,'_v_',v,'.hdf5',sep=''),'phenotypes')
