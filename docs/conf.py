f196d43defb594c6e7f70f5848b84c705adc2456
alextisyoung <alextisyoung@gmail.com>
2017-11-08 09:40:30 +0000

    !

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('/Users/alextisyoung/PycharmProjects/hlmm/hlmm/'))
 sys.path.insert(0, os.path.abspath('/Users/alextisyoung/PycharmProjects/hlmm/'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../hlmm/'))
 print(sys.path)
 
 # -- General configuration ------------------------------------------------
